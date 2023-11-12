e3e418a3aa4c2cf722622d367bc89693e34a1fc4
wawancoding18 <dzakwan112016@gmail.com>
2023-11-12 16:32:36 +0700

    nambah game

--- minigame.js
+++ minigame.js
@@ -1,34 +1,97 @@
-const card = document.getElementById('card');
-const dropZone = document.getElementById('drop-zone');
-const dropZone1 = document.getElementById('drop-zone1');
+document.addEventListener('DOMContentLoaded', function () {
+  const tempatOrganik = document.getElementById('tempat-organik');
+  const tempatAnorganik = document.getElementById('tempat-anorganik');
+  const tempatB3 = document.getElementById('tempat-B3');
 
-card.addEventListener('dragstart', function (event) {
-  console.log(event);
-});
-
-dropZone.addEventListener('dragover', function (event) {
-  event.preventDefault();
-});
-
-dropZone1.addEventListener('dragover', function (event) {
-  event.preventDefault();
-});
-
-dropZone.addEventListener('drop', function (event) {
-  if (card.classList.contains('organik')) {
-    alert('Maaf, salah masuk tempat sampah!');
-  } else {
-    dropZone.prepend(card);
+  // Add event listeners for drag and drop
+  tempatOrganik.addEventListener('dragover', allowDrop);
+  tempatAnorganik.addEventListener('dragover', allowDrop);
+  tempatB3.addEventListener('dragover', allowDrop);
+  
+  tempatOrganik.addEventListener('drop', handleDropOrganik);
+  tempatAnorganik.addEventListener('drop', handleDropAnorganik);
+  tempatB3.addEventListener('drop', handleDropB3);
+  
+  //
+  function allowDrop(event) {
+      event.preventDefault();
   }
-});
-
-dropZone1.addEventListener('drop', function (event) {
-  if (card.classList.contains('anorganik')) {
-    alert('Maaf, salah masuk tempat sampah!');
-  } else {
-    dropZone1.prepend(card);
+  
+  //organik
+  function handleDropOrganik(event) {
+      event.preventDefault();
+      const draggedElement = document.querySelector('.dragging');
+      if (isOrganik(draggedElement)) {
+          tempatOrganik.appendChild(draggedElement);
+          // Handle correct drop for organik waste
+          alert('Sampah Organik berhasil dimasukkan!');
+      } else {
+          // Handle incorrect drop for  waste
+          alert('Salah masuk tempat sampah!');
+      }
   }
-});
+
+  //anorganik
+  function handleDropAnorganik(event) {
+      event.preventDefault();
+      const draggedElement = document.querySelector('.dragging');
+      if (isAnorganik(draggedElement)) {
+          tempatAnorganik.appendChild(draggedElement);
+          // Handle correct drop for anorganik waste
+          alert('Sampah Anorganik berhasil dimasukkan!');
+      } else {
+          // Handle incorrect drop for waste
+          alert('Salah masuk tempat sampah!');
+      }
+  }
+
+
+    //B3
+    function handleDropB3(event) {
+      event.preventDefault();
+      const draggedElement = document.querySelector('.dragging');
+      if (isB3(draggedElement)) {
+          tempatB3.appendChild(draggedElement);
+          // Handle correct drop for B3 waste
+          alert('Sampah B3 berhasil dimasukkan!');
+      } else {
+          // Handle incorrect drop  waste
+          alert('Salah masuk tempat sampah!');
+      }
+  }
+
+
+
+  document.addEventListener('dragstart', function (event) {
+      // Add a class to the dragged element
+      event.target.classList.add('dragging');
+  });
+
+  document.addEventListener('dragend', function (event) {
+      // Remove the class when dragging ends
+      event.target.classList.remove('dragging');
+  });
+
+  
+
+  function isOrganik(element) {
+      // Check if the element is organik waste (you can modify this logic based on your needs)
+      // For example, you can use a specific class name or data attribute
+      return element.src.includes('sampahgame1') || element.src.includes('sampahgame2') || element.src.includes('sampahgame3');
+  }
+  function isAnorganik(element) {
+      // Check if the element is anorganik waste (you can modify this logic based on your needs)
+      // For example, you can use a specific class name or data attribute
+      return element.src.includes('sampahgame4') || element.src.includes('sampahgame5') || element.src.includes('sampahgame6') || element.src.includes('sampahgambar7')
+  }
+  function isB3(element) {
+      // Check if the element is b3 waste (you can modify this logic based on your needs)
+      // For example, you can use a specific class name or data attribute
+      return element.src.includes('sampahgmae8') || element.src.includes('sampahgambar9') || element.src.includes('gambar10');
+  }
+
+})
+
 
 
 
